297d20620e52029d850ae7d7dcf898c978105474
Ameya Gawde <agawde@uwaterloo.ca>
2025-07-05 16:42:54 -0400

    simple loggedin

--- app/src/main/java/com/example/studyio/ui/screens/LoggedInScreen.kt
+++ app/src/main/java/com/example/studyio/ui/screens/LoggedInScreen.kt
@@ -1,43 +1,30 @@
 package com.example.studyio.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
 import com.example.studyio.ui.components.renderClassList
 
 @Composable
 fun LoggedInScreen(navController: NavController) {
-    Column(modifier= Modifier.padding(16.dp)) {
-        Text(text="Hello, Aditya", modifier = Modifier.fillMaxWidth())
-
-        Spacer(modifier= Modifier.height(16.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Column(modifier = Modifier.weight(1f)) {
-                // My classes component
-                renderClassList(navController)
-            }
-
-            Column(modifier = Modifier.weight(1f)) {
-                // Add classes component
-                Text("Add class")
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        // Upcoming deadlines component
-        Text("Upcoming deadlines")
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        Text(
+            text = "Welcome, Aditya 👋",
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold
+        )
     }
 }

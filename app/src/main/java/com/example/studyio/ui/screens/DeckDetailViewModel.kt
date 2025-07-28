842c18c536c2a20cbcc0df03841e13f4fa5dd14e
mnnaegel <86453692+mnnaegel@users.noreply.github.com>
2025-07-28 15:56:29 -0400

    working changes with event emission (#25)

--- app/src/main/java/com/example/studyio/ui/screens/DeckDetailViewModel.kt
+++ app/src/main/java/com/example/studyio/ui/screens/DeckDetailViewModel.kt
@@ -6,9 +6,11 @@ import com.example.studyio.data.entities.Card
 import com.example.studyio.data.entities.CardRepository
 import com.example.studyio.data.entities.Deck
 import com.example.studyio.data.entities.DeckRepository
+import com.example.studyio.events.Events
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,6 +24,15 @@ class DeckDetailViewModel @Inject constructor(
     
     private val _deck = MutableStateFlow<Deck?>(null)
     val deck: StateFlow<Deck?> = _deck
+    
+    init {
+        viewModelScope.launch {
+            Events.deckUpdated.collectLatest {
+                _deck.value?.let { loadDeck(it.id) }
+                _deck.value?.id?.let { loadCards(it) }
+            }
+        }
+    }
 
     suspend fun loadCards(deckId: String) {
             val allCards = cardRepository.getCardsByDeckId(deckId)
